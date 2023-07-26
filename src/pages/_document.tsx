63b4952b8c1861e92e859254b9ab3da46769aacc
brunooomelo <bruno94@outlook.com>
2023-07-26 18:55:00 -0300

    fix meta typo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <title>Bruno Melo Personal Website</title>
-      <meta name="ahrefs-site-verification" content="0ff7ff9de03f88bcd24fe4511f725298dc7d90f3c915636975575d8b463daf90" />
+      <Head title="Bruno Melo Personal Website">
+        <meta name="ahrefs-site-verification" content="0ff7ff9de03f88bcd24fe4511f725298dc7d90f3c915636975575d8b463daf90"></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
