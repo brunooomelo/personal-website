042d6b8976f967a3bb6a1e22bf20c50e0475991d
brunooomelo <bruno94@outlook.com>
2023-07-26 18:49:46 -0300

    add ahref meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <title>Bruno Melo Personal Website</title>
+      <meta name="ahrefs-site-verification" content="0ff7ff9de03f88bcd24fe4511f725298dc7d90f3c915636975575d8b463daf90" />
       <body>
         <Main />
         <NextScript />
