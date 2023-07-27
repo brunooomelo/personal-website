0770ca5020acf49ceab44175bfdaa8cea69ddbbc
brunooomelo <bruno94@outlook.com>
2023-07-27 13:59:34 -0300

    add target blank on link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,13 +4,13 @@ import { SiGithub, SiLinkedin, SiTwitter } from "react-icons/si"
 export const Footer = () => {
   return (
     <div className="w-full flex justify-between">
-      <Link href="https://github.com/brunooomelo" rel="noopener noreferrer">
+      <Link href="https://github.com/brunooomelo" target="_blank" rel="noopener noreferrer">
         <SiGithub size={24} />
       </Link>
-      <Link href="https://www.linkedin.com/in/brunooomelo" rel="noopener noreferrer">
+      <Link href="https://www.linkedin.com/in/brunooomelo" target="_blank" rel="noopener noreferrer">
         <SiLinkedin size={24} />
       </Link>
-      <Link href="https://twitter.com/brunooomelo" rel="noopener noreferrer">
+      <Link href="https://twitter.com/brunooomelo" target="_blank" rel="noopener noreferrer">
         <SiTwitter size={24} />
       </Link>
     </div>
