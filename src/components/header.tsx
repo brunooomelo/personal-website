a98f8f7a7b3892450be0c36b244b04f53c853b5c
brunooomelo <bruno94@outlook.com>
2023-08-12 21:26:46 -0300

    add blog and guestbook link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,40 @@
-import Link from "next/link"
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const pages = [
+  {
+    label: "home",
+    ariaLabel: "Ir para a página inicial",
+    url: "/",
+  },
+  {
+    label: "blog",
+    ariaLabel: "Ir para a página do blog",
+    url: "/blog",
+  },
+  {
+    label: "guestbook",
+    ariaLabel: "Ir para a página do guestbook",
+    url: "/guestbook",
+  },
+];
 
 export const Header = () => {
+  const { route } = useRouter();
   return (
     <header className="w-full h-14 flex items-center">
       <ul className="flex gap-4">
-        <li className="underline">
-          <Link arial-label="Ir para a Home" href="/">
-            HOME
-          </Link>
-        </li>
+        {pages.map((page) => (
+          <li
+            className={`${route === page.url ? "underline" : ""} uppercase`}
+            key={page.label}
+          >
+            <Link aria-label={page.ariaLabel} href={page.url}>
+              {page.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </header>
-  )
-}
+  );
+};
