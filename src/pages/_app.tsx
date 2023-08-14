ea1711f55d7ffbacab0f34d909a26dedd1141776
brunooomelo <bruno94@outlook.com>
2023-08-13 22:56:33 -0300

    add react query in guestbook page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import "@/styles/globals.css";
-import { Space_Mono } from "next/font/google";
-import type { AppProps } from "next/app";
 import Head from "next/head";
+import type { AppProps } from "next/app";
+import { Space_Mono } from "next/font/google";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { DefaultSeo } from "next-seo";
 import { SessionProvider } from "next-auth/react";
 
 import SEO from "../../next-seo.config";
 
 const font = Space_Mono({ subsets: ["latin"], weight: ["400", "700"] });
+const queryClient = new QueryClient();
 
 export default function App({
   Component,
@@ -26,13 +28,15 @@ export default function App({
         ></meta>
       </Head>
       <DefaultSeo {...SEO} />
-      <SessionProvider session={session}>
-        <main
-          className={`max-w-3xl mx-auto ${font.className} p-6 flex flex-col gap-8`}
-        >
-          <Component {...pageProps} />
-        </main>
-      </SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <SessionProvider session={session}>
+          <main
+            className={`max-w-3xl mx-auto ${font.className} p-6 flex flex-col gap-8`}
+          >
+            <Component {...pageProps} />
+          </main>
+        </SessionProvider>
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/guestbook.tsx
+++ src/pages/guestbook.tsx
@@ -1,29 +1,19 @@
 import { NextSeo } from "next-seo";
-import { Header } from "@/components/header";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
 import { SiGithub } from "react-icons/si";
-import { FiSend } from "react-icons/fi";
+import { Header } from "@/components/header";
+import { CommentList } from "@/components/comment-list";
+import { useQuery } from "react-query";
+import { CommentForm } from "@/components/form-comment";
 
-const comments = [
-  {
-    id: 1,
-    username: "Bruno Melo",
-    comment:
-      "Hope y'all are doing great! 😉 Hit me up if you're interested in working together!",
-  },
-  {
-    id: 21,
-    username: "yang Yanr",
-    comment: "Hi Lee, I love the great work you are doing 🤝🏾",
-  },
-  {
-    id: 13,
-    username: "Snorlax",
-    comment: "Thank you for sharing your expertise over the years <3",
-  },
-];
 export default function Guestbook() {
   const { data: session, status } = useSession();
+  const { isLoading, data: comments } = useQuery("comments", () =>
+    fetch("/api/guestbook")
+      .then((res) => res.json())
+      .then((res) => res.docs)
+  );
+
   return (
     <>
       <NextSeo title="Bruno Melo - Guestbook" />
@@ -57,39 +47,10 @@ export default function Guestbook() {
       )}
 
       {!!session && status === "authenticated" && (
-        <div className="flex flex-col gap-1 items-start">
-          <form className="flex w-full gap-2 items-start">
-            <input
-              className="px-4 py-1.5  block w-full border-neutral-300 rounded-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100 focus:outline-none"
-              placeholder="Deixe sua mensagem aqui"
-            />
-            <button
-              type="submit"
-              className="flex w-1/4 items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
-            >
-              <span className="text-sm font-semibold leading-6">Enviar</span>
-              <FiSend className="h-5 w-5" />
-            </button>
-          </form>
-          <button
-            onClick={() => signOut()}
-            className="text-xs text-neutral-700 dark:text-neutral-300 mt-2 mb-6"
-          >
-            Sair do guestbook
-          </button>
-        </div>
+        <CommentForm username={session?.user?.name} />
       )}
 
-      <div className="flex flex-col space-y-1 mb-4">
-        {comments.map((comment) => (
-          <div className="w-full text-sm break-words" key={comment.id}>
-            <span className="text-neutral-600 dark:text-neutral-400 mr-1">
-              {comment.username}:
-            </span>
-            {comment.comment}
-          </div>
-        ))}
-      </div>
+      <CommentList isLoading={isLoading} comments={comments} />
     </>
   );
 }
