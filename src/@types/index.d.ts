a7d8c539501400ab78a8dee865e9f98df22dda01
Bruno Melo <bruno94@outlook.com>
2023-08-25 13:39:05 -0300

    get params from github profile

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,3 +1,5 @@
+import type { DefaultUser } from "next-auth";
+
 export {};
 
 declare global {
@@ -5,4 +7,23 @@ declare global {
     gtag: any;
   }
   var _mongoClientPromise: any;
+}
+
+declare module "next-auth" {
+  interface Session {
+    user?: DefaultUser & {
+      id: string;
+      username: string;
+    };
+  }
+  interface Profile {
+    login: string;
+  }
+}
+
+declare module "next-auth/jwt/types" {
+  interface JWT {
+    uid: string;
+    username: string;
+  }
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,42 @@
 import NextAuth, { AuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GithubProvider, { GithubProfile } from "next-auth/providers/github";
 
 export const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+      profile: async (profile: GithubProfile) => ({
+        id: profile.id.toString(),
+        name: profile.name,
+        email: profile.email,
+        image: profile.avatar_url,
+        username: profile.login,
+      }),
     }),
   ],
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        session.user.id = token.uid;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+    jwt: async ({ user, token, profile, account, session }) => {
+      if (user) {
+        token.uid = user.id;
+      }
+
+      if (profile?.login) {
+        token.username = profile.login;
+      }
+      return token;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
 };
 
 export default NextAuth(authOptions);
